0d18573053dc1805f349cb78aa83f4cda1f3433d
shei_dark <95613937+shei-dark@users.noreply.github.com>
2023-12-13 18:39:57 +0100

    Cleaned code + some code for data prep

---
+++ examples/Pixel_Noise/Convallaria/Evaluation.py
@@ -0,0 +1,68 @@
+import sys
+sys.path.append('../../../')
+import torch
+from matplotlib import pyplot as plt
+from tifffile import imread, imsave
+from sklearn.cluster import KMeans
+import numpy as np
+from glob import glob
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+
+path="/group/jug/Sheida/maester_data/download/high_c1/"
+patch_size = 64
+# model = torch.load("/group/jug/Sheida/HDN models/*.net")
+model = torch.load("/home/sheida.rahnamai/GIT/HDN/examples/Pixel_Noise/Convallaria/Trained_model/model/HDN Muller_best_vae.net")
+model.mode_pred=True
+model.eval()
+model.to(device)
+
+files = sorted(glob(path+"data/test/*.tif"))
+
+img_height,img_width = 699, 760
+for i in files:
+    img = imread(i)
+    img_t = get_normalized_tensor(img,model,device)
+    image_sample = img_t.view(1,1,img_height,img_width)
+    image_sample = image_sample.to(device=device, dtype=torch.float)
+    with torch.no_grad():
+        sample = model(image_sample)
+        for idx in range(len(sample['mu'])):
+            mu = sample['mu'][idx][0].cpu().numpy()
+            output = clustering(idx, mu)
+            print(i)
+            imsave(path+"label/"+str(idx+1)+"/"+i[i.rfind("/"):i.rfind(".")]+".tif", output.reshape(22*(2**(4-idx)), 24*(2**(4-idx))))
+
+def get_normalized_tensor(img,model,device):
+    '''
+    Normalizes tensor with mean and std.
+    Parameters
+    ----------
+    img: array
+        Image.
+    model: Hierarchical DivNoising model
+    device: GPU device.
+    '''
+    test_images = torch.from_numpy(img.copy()).to(device)
+    data_mean = model.data_mean
+    data_std = model.data_std
+    test_images = (test_images-data_mean)/data_std
+    return test_images
+
+def clustering(idx, mu):
+    slice_features_mu = mu.astype(float)
+    feature_flatten_mu = slice_features_mu.reshape(32, -1).T
+    # feature_flatten_mu = slice_features_mu.reshape(32*(2**idx), -1).T
+    # if idx == 0 or idx == 1 or idx == 2:
+    #     feature_flatten_mu = slice_features_mu.reshape(32, -1).T
+    # elif idx == 3 or idx == 4:
+    #     feature_flatten_mu = slice_features_mu.reshape(64, -1).T
+    K_CENTRE = 2
+    kmeans_mu = KMeans(
+        n_clusters=K_CENTRE, init='k-means++', n_init=10,
+        max_iter=1000, random_state=777
+    )
+    kmeans_mu.fit(feature_flatten_mu)
+    labels_mu = kmeans_mu.predict(feature_flatten_mu)
+    return labels_mu
--- examples/Pixel_Noise/Convallaria/Training.py
+++ examples/Pixel_Noise/Convallaria/Training.py
@@ -6,45 +6,40 @@ import torch
 import sys
 sys.path.append('../../../')
 from models.lvae import LadderVAE
-from lib.gaussianMixtureNoiseModel import GaussianMixtureNoiseModel
 from boilerplate import boilerplate
 import lib.utils as utils
 import training
 from tifffile import imread
 from matplotlib import pyplot as plt
 from tqdm import tqdm
+import wandb
+import random
+import tifffile as tiff
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
-print("Using device: ", device)
 
 path="/group/jug/Sheida/maester_data/download/high_c1/"
-observation= imread(path+'high_c1_source.tif')
-
-train_data = observation[:int(0.85*observation.shape[0])]
-val_data= observation[int(0.85*observation.shape[0]):]
+train_data = tiff.imread(path+"data/train/*.tif")
+val_data = tiff.imread(path+"data/validation/*.tif")
+test_data = tiff.imread(path+"data/test/*.tif")
 print("Shape of training images:", train_data.shape, "Shape of validation images:", val_data.shape)
 
-patch_size = 128
-img_width = observation.shape[2]
-img_height = observation.shape[1]
+patch_size = 64
+img_width = 699
+img_height = 760
 num_patches = int(float(img_width*img_height)/float(patch_size**2)*1)
 train_images = utils.extract_patches(train_data, patch_size, num_patches)
-train_images = train_images[:5000] # We limit training patches to 1000 to speed up training but it is not necessary
 val_images = utils.extract_patches(val_data, patch_size, num_patches)
-val_images = val_images[:500] # We limit validation patches to 1000 to speed up training but it is not necessary
-test_images = val_images[:100]
+test_images = utils.extract_patches(test_data, patch_size, num_patches)
 img_shape = (train_images.shape[1], train_images.shape[2])
 print("Shape of training images:", train_images.shape, "Shape of validation images:", val_images.shape)
-
-model_name = "convallaria"
+model_name = "HDN Muller"
 directory_path = "./Trained_model/" 
 
 # Data-specific
 gaussian_noise_std = None
-# noise_model_params= np.load("./data/Convallaria_diaphragm/GMMNoiseModel_convallaria_3_2_calibration.npz")
-noiseModel = None #GaussianMixtureNoiseModel(params = noise_model_params, device = device)
-
+noiseModel = None 
 # Training-specific
 batch_size=128
 virtual_batch = 64
@@ -56,9 +51,14 @@ test_batch_size=100
 # Model-specific
 num_latents = 5
 z_dims = [32]*int(num_latents)
+# z_dims = [32, 64, 128, 256, 512]
 blocks_per_layer = 5
 batchnorm = True
-free_bits = 1.0
+free_bits = 0.0 # if KLD is less than 1 then the loss won't be calculated
+
+debug             = False #[True, False]
+save_output       = True #[True, False]
+project           = 'HDN_Muller'
 
 train_loader, val_loader, test_loader, data_mean, data_std = boilerplate._make_datamanager(train_images,val_images,
                                                                                            test_images,batch_size,
@@ -69,8 +69,33 @@ model = LadderVAE(z_dims=z_dims,blocks_per_layer=blocks_per_layer,data_mean=data
 
 model.train() # Model set in training mode
 
-
 training.train_network(model=model,lr=lr,max_epochs=max_epochs,steps_per_epoch=steps_per_epoch,directory_path=directory_path,
                        train_loader=train_loader,val_loader=val_loader,test_loader=test_loader,
                        virtual_batch=virtual_batch,gaussian_noise_std=gaussian_noise_std,
-                       model_name=model_name,val_loss_patience=30)
+                       model_name=model_name,val_loss_patience=30, debug=debug, save_output=save_output, project_name=project, batch_size=batch_size)
+
+trainHist=np.load(directory_path+"model/train_loss.npy")
+reconHist=np.load(directory_path+"model/train_reco_loss.npy")
+klHist=np.load(directory_path+"model/train_kl_loss.npy")
+valHist=np.load(directory_path+"model/val_loss.npy")
+
+plt.figure(figsize=(18, 3))
+plt.subplot(1,3,1)
+plt.plot(trainHist,label='training')
+plt.plot(valHist,label='validation')
+plt.xlabel("epochs")
+plt.ylabel("loss")
+plt.legend()
+
+plt.subplot(1,3,2)
+plt.plot(reconHist,label='training')
+plt.xlabel("epochs")
+plt.ylabel("reconstruction loss")
+plt.legend()
+
+plt.subplot(1,3,3)
+plt.plot(klHist,label='training')
+plt.xlabel("epochs")
+plt.ylabel("KL loss")
+plt.legend()
+plt.show()
