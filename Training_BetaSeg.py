d28045b92cd858caa70ec0e8eb17e963ff3be85a
shei_dark <95613937+shei-dark@users.noreply.github.com>
2025-02-06 08:59:12 +0100

    ex 23

--- Training_BetaSeg.py
+++ Training_BetaSeg.py
@@ -50,7 +50,7 @@ directory_path = args.directory_path
 
 # Model-specific
 load_checkpoint = True
-checkpoint = "/group/jug/Sheida/HVAE/experiments/19/model_supervised/experiments_best_vae.net"
+checkpoint = "/group/jug/Sheida/HVAE/experiments/20/model_supervised/experiments_best_vae.net"
  
 noiseModel = None
 
