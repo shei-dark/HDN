6675a46b58651f21b94464705b449dea96547eb3
shei_dark <95613937+shei-dark@users.noreply.github.com>
2025-08-16 14:08:11 +0200

    ex 26 only strong pairs

--- lib/utils.py
+++ lib/utils.py
@@ -578,15 +578,15 @@ def multiscale_supervised_cl_semi(
     neg_mask = both_anchors & ~same_label
     
     # weak positives and negatives
-    weak_pos_mask = one_anchor & ~same_group & same_label
-    weak_neg_mask = one_anchor & ~same_group & ~same_label
+    # weak_pos_mask = one_anchor & ~same_group & same_label
+    # weak_neg_mask = one_anchor & ~same_group & ~same_label
     
     # weakest positives and negatives
     # weakest_pos_mask = neither_anchor & ~same_group & same_label
     # weakest_neg_mask = neither_anchor & ~same_group & ~same_label
     
-    pos_mask = pos_mask | weak_pos_mask #| weakest_pos_mask
-    neg_mask = neg_mask | weak_neg_mask #| weakest_neg_mask
+    # pos_mask = pos_mask | weak_pos_mask | weakest_pos_mask
+    # neg_mask = neg_mask | weak_neg_mask | weakest_neg_mask
     
     # --- descriptors (pooled + L2-normalized) ---
     z = torch.cat([F.adaptive_avg_pool2d(x, (1,1)).flatten(1) for x in mus], dim=1)
