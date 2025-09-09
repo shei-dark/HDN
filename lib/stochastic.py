50fd4850ab6ac65bc2bc5e1af6dd2ff142ec4fd9
shei_dark <95613937+shei-dark@users.noreply.github.com>
2025-09-09 09:15:00 +0200

    ex 55

--- lib/stochastic.py
+++ lib/stochastic.py
@@ -201,6 +201,7 @@ class StochasticConvBlock(nn.Module):
                     self._update_temperature()
                     y_pred = y.argmax(dim=1)
                     kl = self._compute_kl(q, p_components, label)
+                    cross_entropy = self._compute_cross_entropy(qy_logits, label)
 
                 if label is None:
                     y = F.softmax(qy_logits, dim=1)
