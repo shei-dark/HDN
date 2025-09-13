6dc827fe25f21fb397a9964b7f7bdc18742a9c24
shei_dark <95613937+shei-dark@users.noreply.github.com>
2025-09-13 23:07:48 +0200

    epsSeg_1

--- boilerplate/dataloader.py
+++ boilerplate/dataloader.py
@@ -41,8 +41,8 @@ class SemisupervisedDataset(Dataset):
         self.n_neighbors = 7  # TODO  # Number of neighbors to sample
         self.seed = 42
         self.rng = random.Random(self.seed)
-        self.samples_per_class: Dict[int, int] = {1: 20}
-        self.default_samples_per_class: int = 10  # TODO
+        self.samples_per_class: Dict[int, int] = {1: 2}
+        self.default_samples_per_class: int = 1  # TODO
         self.groups = self._prepare_metadata()
         self.n_label_per_class = {
             c: len([g for g in self.groups if g["labels"][0] == c])
